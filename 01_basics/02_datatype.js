033b8accbf34ea7f086c1eafecc017afa0714e5a
QuantumDev573 <ashwanimaurya288@gmail.com>
2025-07-18 12:19:42 +0530

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,13 @@
+"Use Strict"; // treat all JS as newer version
+
+// alert("hello") // we are using NodeJS not the browser
+
+let name =  "jack"
+let age = 18
+let isLoggedIn = true
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+console.log(typeof null); // object
