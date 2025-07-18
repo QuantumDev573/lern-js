402e7b2f2208057cc6342111b80a91c8a96a66c9
QuantumDev573 <ashwanimaurya288@gmail.com>
2025-07-18 19:43:34 +0530

    datatype summary

---
+++ 01_basics/datatypes summary.js
@@ -0,0 +1,32 @@
+// primitive 
+
+// 7 types: String, Number, Boolen, Null, Undefined, 
+    Symbol, BigInt
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = true
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+// Reference (Non premitive)
+
+// Array, Object, Function 
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myobj = {
+  name: "HELLO",
+  age: 52
+}
+
+const myFunction = function(){
+  console.log("Hello World");
+}
+
+
