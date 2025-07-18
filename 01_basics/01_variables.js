cc85682d4e07a4e32eee59e7fd810f30a72c5230
QuantumDev573 <ashwanimaurya288@gmail.com>
2025-07-18 10:25:02 +0530

    Create 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "@hfak.com"
+var accountPassword = "123456" // scope problem
+accountCity = "jaipur"
+let accountState;
+
+//  accountId = 1245 not allowed
+
+accountEmail = "@whiph"
+accountPassword = "45852"
+
+console.log(accountId);
+
+console.table([accountState, accountEmail, accountPassword, accountId, accountCity])
+
