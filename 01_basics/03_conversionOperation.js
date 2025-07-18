96ec86b54d727379c3d27bb4525c87c871832dac
QuantumDev573 <ashwanimaurya288@gmail.com>
2025-07-18 13:00:26 +0530

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+
 let score = "33abc"
 
 console.log(typeof(score));
@@ -24,3 +25,26 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof(stringNumber))
+
+
+// Operations //
+
+
+
+let value = 3
+let negvalue = -value
+
+// console.log(negvalue)
+// console.log(2**3) // 2 to the power 3
+
+// let str1 = "hello"
+// let str2 = " Ashwani"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2) // 12
+console.log("1" + 2 + 2) // 122
+console.log(2 + 2 + "1") // 41
+
+console.log(+true); // + use as conversion
