ba85c1b1fe9ce1ecbc110765b6238255693f835f
QuantumDev573 <ashwanimaurya288@gmail.com>
2025-07-19 19:58:37 +0530

    Some string methods

---
+++ 01_basics/05_string.js
@@ -0,0 +1,32 @@
+// const name = 'hello'
+// const repoCount = 40
+
+// // console.log(name + repoCount)
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('hello')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(3))
+// console.log(gameName.indexOf('l'))
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// console.log(gameName.slice(-3, 3))
+
+// const newStringOne = "    hello   "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+const url = "https://hello.com"
+console.log(url.replace('hello', 'Ashwani'))
+console.log(url.includes('sunder'))
+
+console.log(gameName.split('l'))
+
