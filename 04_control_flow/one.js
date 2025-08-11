d64d3832c0a04ec1d9376dda32a196b914683544
QuantumDev573 <ashwanimaurya288@gmail.com>
2025-08-11 20:29:34 +0530

    control flow in js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,48 @@
+// if
+const IsUserLoggedIn = true
+
+temperature = 41
+
+if(temperature){
+  console.log("temperature is less than 50")
+}else{
+   console.log("temperature is less than 50")
+}
+// <, >, <=, >=, ==, ===, !=
+
+const score = 200
+if(score > 100){
+  const power = "fly"
+  console.log(`user ${power}`)
+}
+
+
+// console.log(`user ${power}`) // scope error
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"),console.log("test2");
+
+if(balance < 500){
+  console.log("less than 500")
+}else if(balance < 750){
+  console.log("less than 750")
+}else if(balance < 900){
+  console.log("less than 900")
+}else{
+  console.log("les than 1200")
+}
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromgoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard){
+  console.log("Allow to buy course");
+}
+
+if(loggedInFromEmail || loggedInFromEmail){
+  console.log("User logged in")
+}
