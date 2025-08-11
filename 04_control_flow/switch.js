1a7f6657fc5786b0003f7864bda99db85520e761
QuantumDev573 <ashwanimaurya288@gmail.com>
2025-08-11 20:29:54 +0530

    Switch in java script

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,16 @@
+const month = 6
+switch (month){
+  case 1:
+    return console.log("jan");
+  case 2:
+    return console.log("Feb");
+  case 3:
+    return console.log("march");
+  case 4:
+    return console.log("April");
+  case 5:
+    return console.log("May");
+
+  default:
+    return console.log("Match not Found")
+}
